78df9f3c5e25cb2aff19e0345c92e01d6857ab25
Steven Castroverde <stevencastroverde@Stevens-MacBook-Air.local>
2019-09-17 22:39:31 -0600

    adding tests for button

--- src/components/MButton/MButton.spec.js
+++ src/components/MButton/MButton.spec.js
@@ -12,14 +12,88 @@ describe('MButton', () => {
         expect(wrapper.element).toMatchSnapshot();
     });
 
-    it('creates icon component', () => {
+    it('creates prepend icon component', (done) => {
+        const wrapper = mount(MButton, {
+            propsData: {
+                prependIcon: 'IconAccountBox',
+            },
+        });
+        setTimeout(() => {
+            expect(wrapper.contains(IconAccountBox)).toBe(true);
+            done();
+        })
+    })
+    it('creates append icon component', (done) => {
         const wrapper = mount(MButton, {
             propsData: {
                 appendIcon: 'IconAccountBox',
             },
         });
-        debugger;
-        expect(wrapper.contains(IconAccountBox)).toBe(true);
+        setTimeout(() => {
+            expect(wrapper.contains(IconAccountBox)).toBe(true);
+            done();
+        })
+    });
+    it('renders with icon correctly', (done) => {
+        const wrapper = mount(MButton, {
+            propsData: {
+                prependIcon: 'IconAccountBox',
+            },
+        });
+        setTimeout(() => {
+            expect(wrapper.element).toMatchSnapshot();
+            done();
+        })
+
+    })
+
+    it('adds minimal classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                minimal: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--minimal');
+    });
+    it('adds success classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                success: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--success');
+    })
+    it('adds warning classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                warning: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--warning');
+    })
+    it('adds danger classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                danger: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--danger');
+    })
+    it('adds circular classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                circular: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--circular');
+    });
+    it('adds full width classes', () => {
+        const wrapper = shallowMount(MButton, {
+            propsData: {
+                fullWidth: true,
+            },
+        });
+        expect(wrapper.classes()).toContain('m-button--full-width');
     })
 
 
